99c657abaf5c3d5278f417af7422eef7b60bad5d
JuanGQCadavid <juan.gonzalo@comono.co>
2020-09-26 01:21:53 -0500

    Adding Third party

---
+++ DemoMavenProject/src/main/java/ToolsQA/Calculadora.java
@@ -0,0 +1,15 @@
+package ToolsQA;
+
+public class Calculadora {
+    public Integer addNumbers(int a, int b){
+        return a + b;
+    }
+
+    public Integer multiplyNumbers(int a, int b) {
+        return a * b;
+    }
+
+    public Integer subNumbers(int a, int b){
+        return a - b;
+    }
+}
